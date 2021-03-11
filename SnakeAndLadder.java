7e8c89732eb9480776f86d69a42f442b83a0d6f0
Niharika2404 <niharikang24@gmail.com>
2021-03-11 17:44:56 +0530

    GameStarted: Dice Rolled and New Position are assigned

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,15 +1,34 @@
-
 public class SnakeAndLadder {
-	public static void Player() {
-		SnakeAndLadder p1 = new SnakeAndLadder();
-	}
+		public static final int dice = 6;
+		
+		public static void main(String[] args) {
+		
+			int Position = 0;
+			int NoPlay = 1;
+			int Ladder = 2;
+			int Snake = 3;
+			int NewPosition = 0;
 
-	public static void main(String[] args) {
-		SnakeAndLadder.Player();
-		int Position = 0;
-		double dieCheck = (Math.floor(Math.random() * 10) % 5) + 1;
-		System.out.println("Rolled diced number is: " + dieCheck);
+			double dieCheck = (int)(Math.floor(Math.random() * 10) % 5) + 1;
+				System.out.println(dieCheck);
+					switch (dieCheck) {
+							case 1:
+								NewPosition = 0;
+								System.out.println(NewPosition);
+								break;
+							case 2:
+								NewPosition = (int) (Position + dieCheck);
+								break;
+							case 3:
+								Position--;
+								break;
+							default:
+								System.out.println("Invalid Inputs");
+					}
+			dieCheck++;
+			System.out.println(NewPosition);
 
-	}
-
+		}
 }
+
+
