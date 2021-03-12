ab79b0add5147dab2b08eb60a51efba34be40b61
Niharika2404 <niharikang24@gmail.com>
2021-03-12 22:24:29 +0530

    GameStarted: Player playing till the winning Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,34 +1,158 @@
 public class SnakeAndLadder {
 		public static final int dice = 6;
+		public static final int WinningPosition = 100;
+		public static final int NoPlay  = 1;
+		public static final int Ladder = 2;
+		public static final int Snake = 3;
 		
 		public static void main(String[] args) {
 		
 			int Position = 0;
-			int NoPlay = 1;
-			int Ladder = 2;
-			int Snake = 3;
-			int NewPosition = 0;
-
-			double dieCheck = (int)(Math.floor(Math.random() * 10) % 5) + 1;
-				System.out.println(dieCheck);
-					switch (dieCheck) {
-							case 1:
-								NewPosition = 0;
-								System.out.println(NewPosition);
-								break;
-							case 2:
-								NewPosition = (int) (Position + dieCheck);
-								break;
-							case 3:
-								Position--;
-								break;
-							default:
-								System.out.println("Invalid Inputs");
+			int NewPosition =
+					0;
+			int DiceRolled = 0;
+				
+			int dieCheck = (int)(Math.floor(Math.random() * 10) % 5) + 1;
+				System.out.println("Number came is: " + dieCheck);
+				switch (dieCheck) {
+				case 1:
+					if (NewPosition <= WinningPosition) {
+					
+						int value = (int) (Math.floor(Math.random() * 10) % 3) + 1;
+						System.out.println("The value is: " + value);
+							switch (value) {
+								case 1:
+									NewPosition = 0;
+									System.out.println("Its NoPlay, Player position iss: " + NewPosition);
+									break;
+								case 2:
+									NewPosition = (Position + dieCheck);
+									System.out.println("It is Ladder, Player position is: " + NewPosition);
+									break;
+								case 3:
+									System.out.println("Its Snake, Go back to the starting position: " + Position);
+									break;
+								
+								default:
+									System.out.println("Invalid Inputs");
+							}
 					}
-			dieCheck++;
-			System.out.println(NewPosition);
-
+					case 2:
+						if (NewPosition <= WinningPosition) {
+							
+							int value = (int) (Math.floor(Math.random() * 10) % 3) + 1;
+							System.out.println("The value is: " + value);
+								switch (value) {
+									case 1:
+										NewPosition = 0;
+										System.out.println("Its NoPlay, Player position iss: " + NewPosition);
+										break;
+									case 2:
+										NewPosition = (Position + dieCheck);
+										System.out.println("It is Ladder, Player position is: " + NewPosition);
+										break;
+									case 3:
+										System.out.println("Its Snake, Go back to the starting position: " + Position);
+										break;
+									
+									default:
+										System.out.println("Invalid Inputs");
+								}
+						}
+					case 3:
+						if (NewPosition <= WinningPosition) {
+							
+							int value = (int) (Math.floor(Math.random() * 10) % 3) + 1;
+							System.out.println("The value is: " + value);
+								switch (value) {
+									case 1:
+										NewPosition = 0;
+										System.out.println("Its NoPlay, Player position iss: " + NewPosition);
+										break;
+									case 2:
+										NewPosition = (Position + dieCheck);
+										System.out.println("It is Ladder, Player position is: " + NewPosition);
+										break;
+									case 3:
+										System.out.println("Its Snake, Go back to the starting position: " + Position);
+										break;
+									
+									default:
+										System.out.println("Invalid Inputs");
+								}
+						}
+					case 4:
+						if (NewPosition <= WinningPosition) {
+							
+							int value = (int) (Math.floor(Math.random() * 10) % 3) + 1;
+							System.out.println("The value is: " + value);
+								switch (value) {
+									case 1:
+										NewPosition = 0;
+										System.out.println("Its NoPlay, Player position iss: " + NewPosition);
+										break;
+									case 2:
+										NewPosition = (Position + dieCheck);
+										System.out.println("It is Ladder, Player position is: " + NewPosition);
+										break;
+									case 3:
+										System.out.println("Its Snake, Go back to the starting position: " + Position);
+										break;
+									
+									default:
+										System.out.println("Invalid Inputs");
+								}
+						}
+					case 5:
+						if (NewPosition <= WinningPosition) {
+							
+							int value = (int) (Math.floor(Math.random() * 10) % 3) + 1;
+							System.out.println("The value is: " + value);
+								switch (value) {
+									case 1:
+										NewPosition = 0;
+										System.out.println("Its NoPlay, Player position iss: " + NewPosition);
+										break;
+									case 2:
+										NewPosition = (Position + dieCheck);
+										System.out.println("It is Ladder, Player position is: " + NewPosition);
+										break;
+									case 3:
+										System.out.println("Its Snake, Go back to the starting position: " + Position);
+										break;
+									
+									default:
+										System.out.println("Invalid Inputs");
+								}
+						}
+					case 6:
+						if (NewPosition <= WinningPosition) {
+							
+							int value = (int) (Math.floor(Math.random() * 10) % 3) + 1;
+							System.out.println("The value is: " + value);
+								switch (value) {
+									case 1:
+										NewPosition = 0;
+										System.out.println("Its NoPlay, Player position iss: " + NewPosition);
+										break;
+									case 2:
+										NewPosition = (Position + dieCheck);
+										System.out.println("It is Ladder, Player position is: " + NewPosition);
+										break;
+									case 3:
+										System.out.println("Its Snake, Go back to the starting position: " + Position);
+										break;
+									
+									default:
+										System.out.println("Invalid Inputs");
+								}
+						}
+					default:
+						System.out.println("Dice got missed");
+			}
+			System.out.println("Player is at: " + NewPosition);
 		}
-}
+			
+	}
 
 
